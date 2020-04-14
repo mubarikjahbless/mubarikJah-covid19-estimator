efce3580fc9759096a0cdbd2a383b3121add6e23
mubarikjahbless <alalingajahbless@gmail.com>
2020-04-14 22:14:40 +0000

    start building

--- src/estimator.php
+++ src/estimator.php
@@ -2,24 +2,35 @@
 
 <?php
 
-require ('challengesClass.php');
+require('challengesClass.php');
+
+
+
+$challengeOne_impact = new  impact();
+$challengeOne_SevereImpact = new severeImpact();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $reportedCase = covid19ImpactEstimator($_POST["reportedCase"]);
+  $requestedTime = covid19ImpactEstimator($_POST["timeToElapse"]);
+  $totalHospitalBeds = covid19ImpactEstimator($_POST["totalHospitalBeds"]);
+  $avgDailyIncomeInUSD = covid19ImpactEstimator($_POST["avgDailyIncomeInUSD"]);
+  $avgDailyIncomePopulation = covid19ImpactEstimator($_POST["avgDailyIncomePopulation"]);
+
+  $challengeOne_impact->impact_currentlyInfected($reportedCase, $requestedTime, $totalHospitalBeds, $avgDailyIncomeInUSD, $avgDailyIncomePopulation);
+  $challengeOne_SevereImpact->severe_currentlyInfected($reportedCase, $requestedTime, $totalHospitalBeds, $avgDailyIncomeInUSD, $avgDailyIncomePopulation);
+}
+
+
+
+
 
 
 function covid19ImpactEstimator($data)
 {
-  //
-  $challengeOne_impact= new  impact();
-  $challengeOne_SevereImpact= new severeImpact();
- $reportedCase= $data['reportedCase'];
- $requestedTime=$data['timeToElapse'];
- $totalHospitalBeds= $data['totalHospitalBeds'];
- $avgDailyIncomeInUSD= $data['avgDailyIncomeInUSD'];
- $avgDailyIncomePopulation= $data['avgDailyIncomePopulation'];
- $challengeOne_impact->impact_currentlyInfected($reportedCase,$requestedTime,$totalHospitalBeds,$avgDailyIncomeInUSD,$avgDailyIncomePopulation);
- $challengeOne_SevereImpact->severe_currentlyInfected($reportedCase,$requestedTime,$totalHospitalBeds,$avgDailyIncomeInUSD,$avgDailyIncomePopulation);
- 
-  
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+
+
   return $data;
 }
-
-

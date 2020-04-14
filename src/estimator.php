60e912a392c8089e9da7d044ff6499a2738be365
mubarikjahbless <alalingajahbless@gmail.com>
2020-04-14 21:57:53 +0000

    start building

--- src/estimator.php
+++ src/estimator.php
@@ -10,11 +10,11 @@ function covid19ImpactEstimator($data)
   //
   $challengeOne_impact= new  impact();
   $challengeOne_SevereImpact= new severeImpact();
- $reportedCase= $data->reportedCase;
- $requestedTime=$data->timeToElapse;
- $totalHospitalBeds= $data->totalHospitalBeds;
- $avgDailyIncomeInUSD= $data->avgDailyIncomeInUSD;
- $avgDailyIncomePopulation= $data->avgDailyIncomePopulation;
+ $reportedCase= $data['reportedCase'];
+ $requestedTime=$data['timeToElapse'];
+ $totalHospitalBeds= $data['totalHospitalBeds'];
+ $avgDailyIncomeInUSD= $data['avgDailyIncomeInUSD'];
+ $avgDailyIncomePopulation= $data['avgDailyIncomePopulation'];
  $challengeOne_impact->impact_currentlyInfected($reportedCase,$requestedTime,$totalHospitalBeds,$avgDailyIncomeInUSD,$avgDailyIncomePopulation);
  $challengeOne_SevereImpact->severe_currentlyInfected($reportedCase,$requestedTime,$totalHospitalBeds,$avgDailyIncomeInUSD,$avgDailyIncomePopulation);
  
